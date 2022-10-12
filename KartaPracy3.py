b071daa42f614aa1e3d5f29db285368d2af1a08f
Suppe0 <114559526+KacperOtreba@users.noreply.github.com>
2022-10-12 06:23:53 +0000

    Dodanie zadania 6

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -20,3 +20,54 @@
 
 # for i in range(99,9,-1): print(i, end=" ")
 
+
+
+# pętla liczb dwucyfrowych nieparzystych od 15 do 31  
+
+# pętla liczb dwucyfrowych malejąco (step ujemny)
+# for i in range(99,9,-1): print(i, end=" ")
+
+# pętla liczb dwucyfrowych malejąco (step dodatni)
+# for i in range(10,100,1): print(109-i, end=" ")
+
+# pętla liczb 3-cyfrowych podzielnych przez 20
+# for i in range(100, 1000, 20): print(i, end=" ")
+
+# print("Zadanie 1")
+# n = int(input())
+# for i in range(n):
+#   print(i**3 + 3, end=" ")
+
+# print("Zadanie 2")
+# for i in range(105, 1000, 15):
+#   print(i, end=" ")
+
+# print("Zadanie 3")
+# n = int(input())
+# for i in range(1, n+1):
+#   if n % i == 0:
+#     print(i, end=" ")
+
+# print("Zadanie 4")
+# suma = 0
+# for i in range(10, 100):
+#   suma = suma + i
+# print(suma)
+
+# print("Zadanie 5")
+# n = int(input())
+# suma = n*(n+1)//2
+# for i in range(n-1):
+#   x = int(input())
+#   suma = suma - x
+# print("Nie podałeś:", suma)
+
+print("Zadanie 6")
+n = int(input())
+a = 0
+b = 1
+print(a, end=" ")
+print(b, end=" ")
+for i in range(n-2):
+  a, b = b, a+b
+  print(b, end=" ")
