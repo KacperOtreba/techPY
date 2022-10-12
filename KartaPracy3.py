89b1fab28483a13eece6007ba97872d5a7a6dea2
Suppe0 <114559526+KacperOtreba@users.noreply.github.com>
2022-10-12 05:20:41 +0000

    Dodanie tymczasowe KartaPracy.py

---
+++ KartaPracy3.py
@@ -0,0 +1,22 @@
+# Ankietka
+
+# a = int(input())
+# b = int(input())
+# if (1.3*a > b):
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# print("ZADANIE 1")
+
+# n = int(input())
+# for i in range(n):
+#   print(i**3 + 3,end=" ")
+
+# for i in range(10,22): print(i, end=" ")
+
+
+# for i in range(15,32,2): print(i, end=" ")
+
+# for i in range(99,9,-1): print(i, end=" ")
+
