c98e7aec14e81d5aad41704fd4f8983e886b74b5
Suppe0 <114559526+KacperOtreba@users.noreply.github.com>
2022-10-19 06:08:34 +0000

    wyk. kp3a (zad 1,2)

---
+++ KartaPracy3a.py
@@ -0,0 +1,20 @@
+# print("Zadanie 1")
+# n = int(input())
+# for i in range(n):
+#   print("*-|", end="")
+
+# print("Zadanie 2")
+# n = int(input())
+# for i in range()
+
+# print("*"*5, "*"*3)
+
+# n = int(input())
+# for i in range(1, n+1):
+#   print("*"*i, end="")
+#   if i%2 == 1:
+#     print("||", end="")
+#   else:
+#     print("--", end="")
+
+print("Zadanie 3")
