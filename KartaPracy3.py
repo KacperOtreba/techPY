85939748a10a662ab2cf40e04ae6f35515f7fb8c
Suppe0 <114559526+KacperOtreba@users.noreply.github.com>
2022-10-19 05:48:58 +0000

    dok. kp3

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -72,3 +72,27 @@
 #   a, b = b, a+b
 #   print(b, end=" ")
 
+# POST - utrwalenie pętli
+
+# Pętle for liczb trzycyforowych podzielnych przez 13 (2 sposoby)
+# for i in range(104,1000):
+#   if i % 13 == 0:
+#     print(i, end=" ")
+# #############
+# for i in range(100,1000,13):
+#   print(i, end=" ")
+# # Pętle for liczb dwucyfrowych parzystych (3 sposoby)
+# for i in range(9,100,2):
+#   print(i)
+# ########################
+# for i in range(9,100):
+#   if i % 2 == 0:
+#     print(i, end=" ")
+# ####################
+# for i in range(5,50):
+#   print(i*2, end=" ")
+# Pętle for potęg cyfr: WY: 0, 1, 4, 9, 16 .. 81 (2 sposoby printa)
+
+# for i in range(10):
+#   print(i, "^2=", i**2, sep="")
+
