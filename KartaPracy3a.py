ab8f28f6f3373ccf3b38606544f7152174e5c795
Suppe0 <114559526+KacperOtreba@users.noreply.github.com>
2022-10-26 06:09:35 +0000

    Zrobienie zadan

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -32,3 +32,30 @@
 #   for j in range(1,11):
 #     print(j*i, end="\t")
 #   print()
+
+
+# n = int(input())
+# for i in range(n):
+#   for j in range(i+1):
+#     print("*", end="")
+#   print()
+
+
+# n = int(input())
+# for i in range(n):
+#   for j in range(i+1):
+#     print("*", end="")
+#   print()
+
+# print()
+# print()
+
+# for i in range(n):
+#   for j in range(n-i):
+#     print("*", end="")
+#   print()
+
+print("Zadanie 5")
+n = int(input())
+for i in range(n):
+  if n % 3 == 0:
