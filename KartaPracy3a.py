0cbfcac2b4f82857c4228ddd743ba5b18834ccf8
Suppe0 <114559526+KacperOtreba@users.noreply.github.com>
2022-11-02 07:21:34 +0000

    Dodanie pre

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -1,3 +1,24 @@
+n = int(input())
+# *
+# **
+# ***
+# ****
+
+# ****
+# ***
+# **
+# *
+
+#    *
+#   **
+#  ***
+# ****
+
+# ****
+#  ***
+#   **
+#    *
+
 # print("Zadanie 1")
 # n = int(input())
 # for i in range(n):
@@ -50,12 +71,84 @@
 # print()
 # print()
 
+# n = int(input())
 # for i in range(n):
 #   for j in range(n-i):
 #     print("*", end="")
 #   print()
 
-print("Zadanie 5")
-n = int(input())
+#    *
+#   **
+#  ***
+# ****
+
+# ****
+#  ***
+#   **
+#    *
+
+# n = int(input())
+# for i in range(n):
+#   for j in range(n - i - 1):
+#     print(" ", end=" ")
+#   for k in range(n - i - 1, n):
+#     print("*", end=" ")
+#   print()
+
+
+
+# for i in range(n):
+#   for j in range(i):
+#     print(" ", end=" ")
+#   for k in range(i, n):
+#     print("*", end=" ")
+#   print()
+
 for i in range(n):
-  if n % 3 == 0:
+  for j in range(n):
+    if i >= j:
+      print("*", end=" ")
+    else:
+      print(" ", end=" ")
+  print()  
+
+print()
+print()
+
+for i in range(n):
+  for j in range(n):
+    if i <= j:
+      print("*", end=" ")
+  print()
+
+print()
+print()
+
+for i in range(n):
+  for j in range(n):
+    if i + j == n - 1:
+      print("*", end=" ")
+    else:
+      print(" ", end=" ")
+  print()
+
+print()
+print()
+
+
+for i in range(n):
+  for j in range(n):
+    if i <= n - j - 1:
+      print("*", end=" ")
+    else:
+      print(" ", end=" ")
+  print()  
+
+print()
+
+# punkty (i, j)
+# (1, 1) (1, 2) (1, 3) (1, 4) (1, 5)
+# (2, 1) (2, 2) (2, 3) (2, 4) (2, 5)
+# (3, 1) (3, 2) (3, 3) (3, 4) (3, 5)
+# (4, 1) (4, 2) (4, 3) (4, 4) (4, 5)
+# (5, 1) (5, 2) (5, 3) (5, 4) (5, 5)
